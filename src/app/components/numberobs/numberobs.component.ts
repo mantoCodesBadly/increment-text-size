4c5dce1247d64ee80905302116c43f393f246547
Matteo <mantoan.matteo@cadlinesw.com>
2023-05-26 17:07:00 +0200

    all workin: 1.0

--- src/app/components/numberobs/numberobs.component.ts
+++ src/app/components/numberobs/numberobs.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
+import { Observable, Subscription, fromEvent } from 'rxjs';
+import { map, startWith, scan } from 'rxjs/operators';
 
 @Component({
     selector: 'number-obs',
     templateUrl: './numberobs.component.html',
-    styleUrls: ['./numberobs.component.scss']
+    styleUrls: ['./numberobs.component.scss'],
 })
 export class NumberObsComponent {
-    num: number;
+    incrementClick$ = fromEvent(
+        document /*.getElementById("#increment") as HTMLButtonElement*/,
+        'click'
+    ).pipe(
+        map((event) => +1),
+        scan((acc, incr) => acc + incr)
+    );
+    numToDisplay: number;
 
     constructor() {
-        this.num = 0;
+        this.numToDisplay = 0;
+        this.incrementClick$.subscribe((value) => (this.numToDisplay = value));
+        console.log(document.getElementById('increment') as HTMLElement);
     }
-    
 }
